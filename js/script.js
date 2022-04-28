e5552b6572d5e0ffa38e180e8fbc3e5945836b54
Pietroarmill <pietroarmillotta38@gmail.com>
2022-04-28 16:45:41 +0200

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const team = [
+  {
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    image: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    image: 'angela-caroll-chief-editor.jpg',
+  },
+  {
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    image: 'walter-gordon-office-manager.jpg',
+  },
+  {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg',
+  },
+  {
+    name: 'Scott Estrada',
+    role: 'Developer',
+    image: 'scott-estrada-developer.jpg',
+  },
+  {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image: 'barbara-ramos-graphic-designer.jpg',
+  },
+];
+
+const teamContainer = document.querySelector(".team-container");
+
+for (let i = 0; i < team.length; i++) {
+  const thisElement = team[i];
+  const thisName = thisElement.name;
+  const thisRole = thisElement.role;
+  const thisImage = thisElement.image;
+  console.log("name:", thisElement.name, "ruolo:", thisElement.role, "immagine:", thisElement.image)
+  
+  
+  teamContainer.innerHTML += `<div class="team-card"><div class="card-image"><img src="img/${thisImage}" alt="Wayne Barnett" /></div></div>`
+
+}
