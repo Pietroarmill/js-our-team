cb85b2994eac494aa08d54cc841b80c6247fb178
Pietroarmill <pietroarmillotta38@gmail.com>
2022-04-28 17:10:16 +0200

    completed

--- js/script.js
+++ js/script.js
@@ -33,14 +33,49 @@ const team = [
 
 const teamContainer = document.querySelector(".team-container");
 
+// creo un ciclo per prelevera i dati dall'array e stamparli sulla console e sul documento.
 for (let i = 0; i < team.length; i++) {
+  // qui li prelevo.
   const thisElement = team[i];
   const thisName = thisElement.name;
   const thisRole = thisElement.role;
   const thisImage = thisElement.image;
-  console.log("name:", thisElement.name, "ruolo:", thisElement.role, "immagine:", thisElement.image)
-  
-  
-  teamContainer.innerHTML += `<div class="team-card"><div class="card-image"><img src="img/${thisImage}" alt="${thisName}" /></div><div class="card-text"><h3>${thisName}</h3><p>${thisRole}</p></div></div>`
+
+  // qui li stampo sulla console.
+  console.log("name:", thisElement.name);
+  console.log("ruolo:", thisElement.role);
+  console.log("immagine:", thisElement.image);
+  console.log("");
+
+  // qui li stampo sul documento.
+  teamContainer.innerHTML += `<div class="team-card"><div class="card-image"><img src="img/${thisImage}" alt="${thisName}" /></div><div class="card-text"><h3>${thisName}</h3><p>${thisRole}</p></div></div>`;
 
 }
+
+const addBtn = document.getElementById("addMemberButton");
+
+addBtn.addEventListener("click", function() {
+
+  const userName = document.getElementById("name").value;
+  const userRole = document.getElementById("role").value;
+  const userImage = document.getElementById("image").value;
+
+  const userCard = {
+    name: userName,
+    role: userRole,
+    image: userImage
+  }
+
+  team.push(userCard);
+
+  console.log(team);
+
+
+  const teamContainer = document.querySelector(".team-container");
+
+ 
+  teamContainer.innerHTML += `<div class="team-card"><div class="card-image"><img src="img/${userImage}" alt="${userName}" /></div><div class="card-text"><h3>${userName}</h3><p>${userRole}</p></div></div>`;
+  
+
+
+})
